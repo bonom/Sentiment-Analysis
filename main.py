ff00b400e18f0fdf8ca5a8a922fe5ef9cc86038c
bonom <andrea.bonomi489@gmail.com>
2023-01-07 15:04:52 +0100

    Update parameters

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import torch
 from classes.commons import check_downloads
 from classes.baseline import main as baseline_main
 
-from classes.improved import train_subjectivity_classification, train_polarity_classification
+from classes.personal_implementation import train_subjectivity_classification, train_polarity_classification
+from classes.paper_implementation import train_subjectivity_classification as train_subjectivity_classification_paper, train_polarity_classification as train_polarity_classification_paper
 
 if __name__ == '__main__':
     check_downloads()
@@ -15,13 +16,18 @@ if __name__ == '__main__':
     ### Custom classifiers
     print(f"--- Running custom classifiers ---")
     # Hyperparameters
-    epochs = 40
-    lr = 0.001
-    weight_decay = 0.0001
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    # Train subjectivity classifier
-    subj_class = train_subjectivity_classification(epochs, lr, weight_decay, device)
+    # Train subjectivity classifier with custom implementation
+    subj_class = train_subjectivity_classification(device=device)
 
-    # Train polarity classifier
-    pol_class = train_polarity_classification(epochs, lr, weight_decay, device)
+    # Train polarity classifier with custom implementation
+    pol_class = train_polarity_classification(device=device)
+
+    # Train subjectivity classifier with custom implementation
+    # subj_class = train_subjectivity_classification_paper(device=device)
+
+    # Train polarity classifier with custom implementation
+    # pol_class = train_polarity_classification_paper(device=device)
+
+    
