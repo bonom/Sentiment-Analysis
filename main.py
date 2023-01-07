50935412a779f544b44b43af0cc96ef07c8a2492
bonom <andrea.bonomi489@gmail.com>
2023-01-07 11:24:54 +0100

    Update main

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#! /usr/bin/env python3
 import torch
 from classes.commons import check_downloads
 from classes.baseline import main as baseline_main
@@ -13,4 +14,14 @@ if __name__ == '__main__':
 
     ### Custom classifiers
     print(f"--- Running custom classifiers ---")
-    train_subjectivity_classification(40, 0.001, 0.0001, 'cpu' if not torch.cuda.is_available() else 'cuda:0')
+    # Hyperparameters
+    epochs = 40
+    lr = 0.001
+    weight_decay = 0.0001
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    # Train subjectivity classifier
+    subj_class = train_subjectivity_classification(epochs, lr, weight_decay, device)
+
+    # Train polarity classifier
+    pol_class = train_polarity_classification(epochs, lr, weight_decay, device)
