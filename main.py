a2b0e5f5ff23525d3d9818e5db7e06a25d5492ae
bonom <andrea.bonomi489@gmail.com>
2023-01-13 19:59:28 +0100

    Add exception handler

--- main.py
+++ main.py
@@ -7,30 +7,34 @@ from classes.personal_implementation import train_subjectivity_classification, t
 from classes.paper_implementation import train_subjectivity_classification as train_subjectivity_classification_paper, train_polarity_classification as train_polarity_classification_paper
 
 if __name__ == '__main__':
-    check_downloads()
-    # Hyperparameters
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    try:
+        check_downloads()
+        # Hyperparameters
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    ### Baseline classifiers
-    print(f"--- Running baseline classifiers ---")
-    # baseline_main()
-    
-    ### Custom classifiers
-    print(f"--- Running paper classifiers ---")
+        ### Baseline classifiers
+        print(f"--- Running baseline classifiers ---")
+        # baseline_main()
+        
+        ### Custom classifiers
+        print(f"--- Running paper classifiers ---")
 
-    # Train subjectivity classifier with custom implementation
-    subj_class = train_subjectivity_classification_paper(epochs=100, device=device)
+        # Train subjectivity classifier with custom implementation
+        subj_class = train_subjectivity_classification_paper(epochs=100, device=device)
 
-    # Train polarity classifier with custom implementation
-    pol_class = train_polarity_classification_paper(epochs=100, device=device)
+        # Train polarity classifier with custom implementation
+        pol_class = train_polarity_classification_paper(epochs=100, device=device)
 
-    ### Custom classifiers
-    print(f"--- Running custom classifiers ---")    
+        ### Custom classifiers
+        print(f"--- Running custom classifiers ---")    
 
-    # Train subjectivity classifier with custom implementation
-    subj_class = train_subjectivity_classification(epochs=100, device=device)
+        # Train subjectivity classifier with custom implementation
+        subj_class = train_subjectivity_classification(epochs=100, device=device)
 
-    # Train polarity classifier with custom implementation
-    pol_class = train_polarity_classification(epochs=100, device=device)
-    
+        # Train polarity classifier with custom implementation
+        pol_class = train_polarity_classification(epochs=100, device=device)
+    except Exception as e:
+        torch.cuda.empty_cache()
+        raise e
+
 
